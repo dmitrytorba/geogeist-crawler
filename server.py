4fbdf294e319525bf6417352a366f6dbefd9dd86
Dmitry Torba <dmitrytorba@gmail.com>
2018-05-11 09:26:53 -0700

    fix encoding error

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request 
 import geo
 import json
+import numpy
 app = Flask(__name__)
 
 @app.route('/')
@@ -11,9 +12,21 @@ def main_page():
 def send_static(path):
     return send_from_directory('static', path)
 
+# https://stackoverflow.com/questions/27050108/convert-numpy-type-to-python
+class NumpyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, numpy.integer):
+            return int(obj)
+        elif isinstance(obj, numpy.floating):
+            return float(obj)
+        elif isinstance(obj, numpy.ndarray):
+            return obj.tolist()
+        else:
+            return super(NumpyEncoder, self).default(obj)
+
 @app.route('/data')
 def data():
     lon = float(request.args.get('lon'))
     lat = float(request.args.get('lat'))
     # TODO: check input
-    return json.dumps(geo.get_data(lat, lon))
+    return json.dumps(geo.get_data(lat, lon),cls=NumpyEncoder)
