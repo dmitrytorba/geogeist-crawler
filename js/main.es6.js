440de614d7f6b7a11c4a338aed3519731024c6d5
Dmitry Torba <dmitrytorba@gmail.com>
2018-05-20 08:34:41 -0700

    clean up ui

--- js/main.es6.js
+++ js/main.es6.js
@@ -1,10 +1,24 @@
-function tileHtml(title, subtitle) {
+function censusTileHtml(conf) {
     var html = `
       <div class="tile is-parent">
       <article class="tile is-child box">
         <div class="content">
-         <p class="title">${title}</p>
-         <p class="subtitle">${subtitle}</p>
+         <p class="title">${conf.title}</p>
+         <p class="subtitle">Population: ${conf.population}</p>
+         <p class="subtitle">Houses: ${conf.houses}</p>
+        </div>
+      </article>
+      </div>`
+    
+    return html
+}
+
+function coordTileHtml(conf) {
+    var html = `
+      <div class="tile is-parent">
+      <article class="tile is-child box">
+        <div class="content">
+         <p class="subtitle">Coordinates: ${conf.lat}, ${conf.lon}</p>
         </div>
       </article>
       </div>`
@@ -22,21 +36,51 @@ if (navigator.geolocation) {
     $.get("/data", render);
 }
 
+function renderCounty(data) {
+    var title = data.county.name + ' County'
+    var html = censusTileHtml({
+	'title': title,
+	'population': data.county.population.total,
+	'houses': data.county.houses
+    })
+    $(".tile-area").append(html)
+}
+
+function renderPlace(data) {
+    if (data.place) {
+	var html = censusTileHtml({
+	    'title': data.place.name,
+	    'population': data.place.population.total,
+	    'houses': data.place.houses
+	})
+	$('.tile-area').append(html)
+    }
+}
+
+function renderTract(data) {
+    if (data.tract) {
+	var title = 'Tract # ' + data.tract.name
+	var html = censusTileHtml({
+	    'title': title,
+	    'population': data.tract.population.total,
+	    'houses': data.tract.houses
+	})
+	$('.tile-area').append(html)
+    }
+}
+
 function render(res) {
     var data = JSON.parse(res)
-    $(".tile-area").append(tileHtml(data.county.name, data.county.population));
-    if (data.place) {
-	$(".tile-area").append(tileHtml(data.place.name, data.place.population));
-    }
-    if (data.tract) {
-	$(".tile-area").append(tileHtml(data.tract.name, data.tract.population));
-    }
+    renderCounty(data)
+    renderPlace(data)
+    renderTract(data)
 }
 
 function showPosition(position) {
     coords.lat = position.coords.latitude; 
     coords.lon = position.coords.longitude;
-    $(".tile-area").append(tileHtml(coords.lat, coords.lon));
+    var html = coordTileHtml(coords)
+    $(".tile-area").append(html)
 
     $.get("/data", {
 	lat: coords.lat,
