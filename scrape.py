894a2b19f6bc529e4e4ef23d126187989540f0f7
Dmitry Torba <dmitrytorba@gmail.com>
2019-01-18 12:25:53 -0800

    find county here

--- scrape.py
+++ scrape.py
@@ -5,35 +5,37 @@ import json
 conn = psycopg2.connect(user='geogeist', password='password',
                         host='localhost', port='5432')
 
-def insert_county():
+def insert_counties():
 	state_fips = '06'
 	dt = geo.get_county_data(state_fips)
-	alameda = dt.iloc[0]
-	geog = alameda.geometry.__geo_interface__
 
 	cur = conn.cursor()
 
-	geog["crs"] = {
-		"type": "name",
-		"properties": {
-			"name": "EPSG:3857"
+	for index, row in dt.iterrows():
+		geog = row.geometry.__geo_interface__
+
+
+		geog["crs"] = {
+			"type": "name",
+			"properties": {
+				"name": "EPSG:3857"
+			}
 		}
-	}
-	geog = json.dumps(geog)
-	cur.execute("INSERT into counties (state, name, geog) VALUES (%s, %s, ST_Multi(ST_Transform(ST_GeomFromGeoJSON(%s),4326)))",
-		(state_fips, alameda.BASENAME, geog))
+		geog = json.dumps(geog)
+		cur.execute("INSERT into counties (state, name, geog) VALUES (%s, %s, ST_Multi(ST_Transform(ST_GeomFromGeoJSON(%s),4326)))",
+			(state_fips, row.BASENAME, geog))
 	#cur.execute("SELECT ")
 
 	conn.commit()
 	cur.close()
 
-def test_distance():
+def get_county():
 	cur = conn.cursor()
-	cur.execute("SELECT ST_Distance('SRID=4326;POINT(-121.2273314 38.6950877)'::geography, c.geog) FROM counties c")
+	cur.execute("SELECT c.name WHERE ST_Covers(c.geog, 'SRID=4326;POINT(-121.2273314 38.6950877)'::geography) FROM counties c")
 	print(cur.fetchone())
 	conn.commit()
 	cur.close()
 
-test_distance()
+get_county()
 
 conn.close()
